ecefe3203d0f4c5c8d3f9bb985291f4219a3e721
Rakshana0802 <49952408+Rakshana0802@users.noreply.github.com>
2020-06-09 22:51:44 +0530

    Create Is Subsequence.cpp

---
+++ Is Subsequence.cpp
@@ -0,0 +1,26 @@
+class Solution
+{
+public:
+    bool isSubsequence(string s, string t)
+    {
+        if (s.size() <= 0)
+            return true;
+        
+        int ps=0, pt=0;
+        while (pt < t.size())
+        {
+            if (s[ps] == t[pt])
+            {
+                ps++; pt++;
+                if (ps >= s.size())
+                    return true;
+            }
+            else
+            {
+                pt++;
+            }
+        }
+        
+        return false;
+    }
+};
