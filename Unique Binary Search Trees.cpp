098652b635fd4da5575cdc71b41250af653a41a9
Rakshana Bagavathi <49952408+Rakshana0802@users.noreply.github.com>
2020-06-24 21:54:03 +0530

    Create Unique Binary Search Trees.cpp

---
+++ Unique Binary Search Trees.cpp
@@ -0,0 +1,17 @@
+class Solution
+{
+public:
+   int numTrees(int n) 
+   {
+      vector <int> dp(n+1);
+      dp[0] = 1;
+      for(int i =1;i<=n;i++)
+      {
+         for(int j = 0;j<i;j++)
+         {
+            dp[i] += (dp[i-1-j] * dp[j]);
+         }
+      }
+      return dp[n];
+   }
+};
